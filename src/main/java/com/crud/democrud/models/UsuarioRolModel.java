e9a151bf26920f14bbd34ad7bcabbf6a348d6ca8
Geraldine <Gerald.e2309@gmail.com>
2022-04-28 00:52:07 -0500

    Creación de entidad Usuario rol

--- src/main/java/com/crud/democrud/models/UsuarioRolModel.java
+++ src/main/java/com/crud/democrud/models/UsuarioRolModel.java
@@ -1,7 +1,48 @@
 package com.crud.democrud.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "rolUsuario")
 public class UsuarioRolModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long idRol;
 
+    private Long idUsuario;
+    private String rol;
 
+    public Long getIdUsuario() {
+        return idUsuario;
+    }
 
+    public void setIdUsuario(Long idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public Long getIdRol() {
+        return idRol;
+    }
+
+    public void setIdRol(Long idRol) {
+        this.idRol = idRol;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public UsuarioRolModel(Long idUsuario, String rol) {
+        this.idUsuario = idUsuario;
+
+        this.rol = rol;
+    }
+
+    public UsuarioRolModel() {
+    }
 }
